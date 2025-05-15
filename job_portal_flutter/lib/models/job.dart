6f558f552248f61e849e33e269101659fb5ec30a
AmnaZubair123 <93050915+AmnaZubair123@users.noreply.github.com>
2025-05-15 15:42:08 +0500

    Add job_portal_flutter/lib/models/job.dart 1747305723886

---
+++ job_portal_flutter/lib/models/job.dart
@@ -0,0 +1,15 @@
+class Job {
+  final int id;
+  final String title;
+  final String company;
+  final String location;
+  final String description;
+
+  Job({
+    required this.id,
+    required this.title,
+    required this.company,
+    required this.location,
+    required this.description,
+  });
+}
